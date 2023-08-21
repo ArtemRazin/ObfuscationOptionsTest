945c6c6a46ca5709febcdded17e75c399d983742
Artem Razin <razin@softanics.com>
2023-08-21 18:04:26 +0300

    Initial files

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ObfuscationOptionsTest;
+
+class Program
+{
+    private int _someField;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    static void GenericMethod<T>() where T: new()
+    {
+        var x = new T();
+        Console.WriteLine("Hello, World!");
+    }
+}
